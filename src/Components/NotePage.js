3f3965a056ab967bc8e4da953f85698be5d8283d
leonardo <106268245+leoz11@users.noreply.github.com>
2024-09-15 22:39:46 -0300

    Add files via upload

--- src/Components/NotePage.js
+++ src/Components/NotePage.js
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { FaArrowLeft, FaEdit, FaCheck, FaUndo, FaRedo } from 'react-icons/fa';
 import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import './Quill.css'
 
 const HeaderWrapper = styled.div`
   position: sticky;
